3022337748a296dd59d9f4d232a5184a929f328c
deepti249 <deepti24.lodha@gmail.com>
2025-06-09 18:53:24 +0530

    commit

---
+++ src/ex_01_Java_Basic/Deepti$.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Deepti$ {
+    public static void main(String[] args) {
+        System.out.println("Hi...I am a class starting with alphabets and having $ symbol");
+    }
+}
