df9b1fb85af0d1df1cf6d60e41ae62734906fa02
deepti249 <deepti24.lodha@gmail.com>
2025-06-09 18:47:44 +0530

    commit

---
+++ src/ex_01_Java_Basic/$123.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class $123 {
+    public static void main(String[] args) {
+        System.out.println("Hi...I am a class starting with $ symbol and having digits");
+    }
+}
