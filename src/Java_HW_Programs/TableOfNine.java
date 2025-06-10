1c3df6b6ed5d67ba599bd0aecb4a7cf59a54b3de
deepti249 <deepti24.lodha@gmail.com>
2025-06-10 20:38:03 +0530

    commit

---
+++ src/Java_HW_Programs/TableOfNine.java
@@ -0,0 +1,28 @@
+package Java_HW_Programs;
+
+public class TableOfNine {
+    public static void main(String[] args) {
+        int num = 9;
+        int i = 1;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+        i++;
+        System.out.printf("%d * %d = %d\n", num, i, num * i);
+
+    }
+}
