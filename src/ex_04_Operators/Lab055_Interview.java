896cd3fb48bfe80b98f9a68a0af635cf259c9ee6
deepti249 <deepti24.lodha@gmail.com>
2025-06-11 16:11:51 +0530

    commit

---
+++ src/ex_04_Operators/Lab055_Interview.java
@@ -0,0 +1,7 @@
+package ex_04_Operators;
+
+public class Lab055_Interview {
+    public static void main(String[] args) {
+        System.out.println('A' == 65);  //ASCII value of A is 65 -> 65==65 -> true
+    }
+}
