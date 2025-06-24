67e5cde49d9b9c8d6d0a519e88194d49aa7ee86d
deepti249 <deepti24.lodha@gmail.com>
2025-06-24 21:16:11 +0530

    commit

---
+++ src/Test_Coding_Attempt2/Task_Arithematic_Assignment_OP.java
@@ -0,0 +1,16 @@
+package Test_Coding_Attempt2;
+
+public class Task_Arithematic_Assignment_OP {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("Arithmetic Operators:");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+
+    }
+}
---
+++ src/Test_Coding_Attempt2/Task_Comparision_Logical_OP.java
@@ -0,0 +1,24 @@
+package Test_Coding_Attempt2;
+
+public class Task_Comparision_Logical_OP {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        // Relational (Comparison) Operators
+        System.out.println("\nRelational Operators:");
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+
+        // Logical Operators
+        boolean x = true, y = false;
+        System.out.println("\nLogical Operators:");
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+
+    }
+}
