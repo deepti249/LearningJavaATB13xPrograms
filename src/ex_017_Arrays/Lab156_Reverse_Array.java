ab60d1c85fa2536c7708eb7487f6f756e9add111
deepti249 <deepti24.lodha@gmail.com>
2025-06-27 09:09:52 +0530

    commit

---
+++ src/ex_017_Arrays/Lab156_Reverse_Array.java
@@ -0,0 +1,12 @@
+package ex_017_Arrays;
+
+public class Lab156_Reverse_Array {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        // 5,4,3,2,1
+
+        for (int i = numbers.length - 1 ; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
